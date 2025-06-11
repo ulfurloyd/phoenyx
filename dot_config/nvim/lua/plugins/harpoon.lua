e9b4265fb258e8ba56c13e341b77e082f52371df
ulfurloyd <eklavyasood@gmail.com>
2025-06-11 22:35:38 +0530

    feat: harpoon changes, added telescope-ui-select nvim

--- dot_config/nvim/lua/plugins/harpoon.lua
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -8,10 +8,10 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-9>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-0>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-->", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-=>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
