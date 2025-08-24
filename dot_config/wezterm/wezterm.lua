93a5f35204ab0312f62867f16d5bba52629fd64c
ulfurloyd <eklavyasood@gmail.com>
2025-08-24 17:29:41 +0530

    feat: temp move to wezterm over kitty

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+-----------------------------------------------------
+local wezterm = require 'wezterm'
+
+-----------------------------------------------------
+-- start of the config block
+local cfg = wezterm.config_builder()
+
+-----------------------------------------------------
+-- font settings
+cfg.font = wezterm.font 'Fira Code Nerd Font Mono'
+cfg.font_size = 13
+
+-----------------------------------------------------
+-- appearance settings
+cfg.window_decorations = "NONE"
+cfg.use_fancy_tab_bar = true
+cfg.tab_bar_at_bottom = true
+cfg.hide_tab_bar_if_only_one_tab = true
+cfg.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+}
+
+-----------------------------------------------------
+-- misc settings
+cfg.max_fps = 120
+-- cfg.prefer_egl = true
+
+return cfg
