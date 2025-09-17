30d56a45996b4d791769bdc6965f53fe2840b5b4
ulfurloyd <eklavyasood@gmail.com>
2025-09-18 01:21:49 +0530

    feat: wezterm config change

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -----------------------------------------------------
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -----------------------------------------------------
 -- start of the config block
@@ -7,7 +7,7 @@ local cfg = wezterm.config_builder()
 
 -----------------------------------------------------
 -- font settings
-cfg.font = wezterm.font 'Fira Code Nerd Font Mono'
+cfg.font = wezterm.font("Fira Code Nerd Font Mono")
 cfg.font_size = 13
 
 -----------------------------------------------------
@@ -17,15 +17,16 @@ cfg.use_fancy_tab_bar = true
 cfg.tab_bar_at_bottom = true
 cfg.hide_tab_bar_if_only_one_tab = true
 cfg.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 -----------------------------------------------------
 -- misc settings
 cfg.max_fps = 120
+cfg.window_close_confirmation = "NeverPrompt"
 -- cfg.prefer_egl = true
 
 return cfg
