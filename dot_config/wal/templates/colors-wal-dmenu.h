5388a68ea4d2e1ad207ea122b960ecc2709c64a6
ulfurloyd <eklavyasood@gmail.com>
2025-10-18 17:37:36 +0530

    feat: pywal dmenu template

---
+++ dot_config/wal/templates/colors-wal-dmenu.h
@@ -0,0 +1,8 @@
+static const char *colors[SchemeLast][2] = {{
+	/*     fg         bg       */
+	[SchemeNorm] = {{ "{color15}", "{color0}" }},
+	[SchemeSel] = {{ "{color15}", "{color1}" }},
+	[SchemeOut] = {{ "{color15}", "{color14}" }},
+	[SchemeSelHighlight] = {{ "{color2}", "{color1}" }},
+	[SchemeNormHighlight] = {{ "{color2}", "{color0}" }},
+}};
