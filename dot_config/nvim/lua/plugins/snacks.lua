7215ac6907999e37af6f7d717a86646fccee28f8
ulfurloyd <eklavyasood@gmail.com>
2026-04-17 13:26:38 +0530

    feat: nvim obsidian plugin, snacks.dashboard change, sesh change

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -34,7 +34,17 @@ return {
           ▄█▄           ▀▄  ▀▀▀▀▀▀▀▀  ▄▀   
          █▀▀█████████▀▀▀▀████████████▀     
          ████▀  ███▀      ▀███  ▀██▀       
-			  ]]
+			    ]],
+			    keys = {
+                    { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                    { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                    { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                    { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+					{ icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cdw = '~/.local/share/chezmoi/dot_config/nvim'})" },
+                    { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                    { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
 			},
 		},
 		explorer = { enabled = true },
@@ -47,8 +57,5 @@ return {
 		scroll = { enabled = true },
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
-	},
-	keys = {
-
 	},
 }
