c05605b8a6dbcf8c37d66a7213d5cb61a693dc06
ulfurloyd <eklavyasood@gmail.com>
2026-04-17 19:05:36 +0530

    feat: tmux-floax and sesh

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,4 @@ vim.keymap.set('n', '-', '<cmd>Oil --float<CR>', { desc = "Open Oil file manager
 vim.keymap.set('n', '<leader>wv', '<C-w>v<CR>', { desc = "Window: Open [v]ertical split" })
 vim.keymap.set('n', '<leader>ws', '<C-w>s<CR>', { desc = "Window: Open horizontal [s]plit" })
 vim.keymap.set('n', '<leader>wc', '<C-w>c<CR>', { desc = "Window: [c]lose split" })
+vim.keymap.set('n', '<leader>wr', '<C-w>r<CR>', { desc = "Window: [r]otate windows downwards/righwards" })
