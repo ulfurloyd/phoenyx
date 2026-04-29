1299492b204abab2a86d1ce43a5210bb42ffa771
ulfurloyd <eklavyasood@gmail.com>
2026-04-30 02:06:30 +0530

    style: opacity since niri got blur PR

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -14,6 +14,7 @@ config.font = wezterm.font("JetBrains Mono")
 -- appearance
 config.initial_cols = 130
 config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.8
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
 	left = 2,
