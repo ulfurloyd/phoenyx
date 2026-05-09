82c4492e5dc506f6e0f1c6194243c2968bbc2b8b
ulfurloyd <eklavyasood@gmail.com>
2026-05-09 16:26:49 +0530

    nvim: proper gofmt implementation through conform

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,18 +1,23 @@
 return {
 	"stevearc/conform.nvim",
+	event = { "BufWritePre", "BufNewFile" },
+	cmd = { "ConformInfo" },
+	keys = { {
+		"<leader>cf",
+		function()
+			require("conform").format()
+		end,
+		desc = "[C]ode: [f]ormat file",
+	} },
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			bash = { "shfmt" },
+			go = { "gofmt" },
 		},
 		format_on_save = {
 			timeout_ms = 500,
 			lsp_format = "fallback",
 		},
-	},
-	keys = {
-		vim.keymap.set("n", "<leader>cf", function()
-			require("conform").format()
-		end, { desc = "[C]ode: [f]ormat file" }),
 	},
 }
--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,38 +1,40 @@
 return {
 	-- Main LSP Configuration
-	'neovim/nvim-lspconfig',
+	"neovim/nvim-lspconfig",
 	dependencies = {
 		{
-			'mason-org/mason.nvim',
+			"mason-org/mason.nvim",
 			---@module 'mason.settings'
 			---@type MasonSettings
 			---@diagnostic disable-next-line: missing-fields
 			opts = {},
 		},
 		-- Maps LSP server names between nvim-lspconfig and Mason package names.
-		'mason-org/mason-lspconfig.nvim',
-		'WhoIsSethDaniel/mason-tool-installer.nvim',
+		"mason-org/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 		-- Useful status updates for LSP.
 		-- { 'j-hui/fidget.nvim', opts = {} },
 	},
 	config = function()
-		vim.api.nvim_create_autocmd('LspAttach', {
-			group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 			callback = function(event)
 				local map = function(keys, func, desc, mode)
-					mode = mode or 'n'
-					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+					mode = mode or "n"
+					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
 
-				map('<leader>cr', vim.lsp.buf.rename, 'Code: [R]ename')
-				map('gl', function() vim.diagnostic.open_float() end, 'Code: Diagnostics in F[L]oat')
-				map('<leader>ca', require('fzf-lua').lsp_code_actions, 'Code: Code [A]ction', { 'n', 'x' })
-				map('<leader>cD', require('fzf-lua').lsp_declarations, 'Code: [D]eclaration')
-				map('<leader>cd', require('fzf-lua').lsp_definitions, 'Code: [d]efinitions')
-				map('<leader>cR', require('fzf-lua').lsp_references, 'Code: [R]eferences')
-				map('<leader>cS', require('fzf-lua').lsp_live_workspace_symbols, 'Code: Workspace [S]ymbols')
-				map('<leader>ct', require('fzf-lua').lsp_typedefs, 'Code: [T]ype Definitions')
+				map("<leader>cr", vim.lsp.buf.rename, "Code: [R]ename")
+				map("gl", function()
+					vim.diagnostic.open_float()
+				end, "Code: Diagnostics in F[L]oat")
+				map("<leader>ca", require("fzf-lua").lsp_code_actions, "Code: Code [A]ction", { "n", "x" })
+				map("<leader>cD", require("fzf-lua").lsp_declarations, "Code: [D]eclaration")
+				map("<leader>cd", require("fzf-lua").lsp_definitions, "Code: [d]efinitions")
+				map("<leader>cR", require("fzf-lua").lsp_references, "Code: [R]eferences")
+				map("<leader>cS", require("fzf-lua").lsp_live_workspace_symbols, "Code: Workspace [S]ymbols")
+				map("<leader>ct", require("fzf-lua").lsp_typedefs, "Code: [T]ype Definitions")
 
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
@@ -40,25 +42,25 @@ return {
 				--
 				-- When you move your cursor, the highlights will be cleared (the second autocommand).
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client:supports_method('textDocument/documentHighlight', event.buf) then
-					local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-					vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+				if client and client:supports_method("textDocument/documentHighlight", event.buf) then
+					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 						buffer = event.buf,
 						group = highlight_augroup,
 						callback = vim.lsp.buf.document_highlight,
 					})
 
-					vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 						buffer = event.buf,
 						group = highlight_augroup,
 						callback = vim.lsp.buf.clear_references,
 					})
 
-					vim.api.nvim_create_autocmd('LspDetach', {
-						group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+					vim.api.nvim_create_autocmd("LspDetach", {
+						group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
 						callback = function(event2)
 							vim.lsp.buf.clear_references()
-							vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+							vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
 						end,
 					})
 				end
@@ -67,40 +69,14 @@ return {
 				-- code, if the language server you are using supports them
 				--
 				-- This may be unwanted, since they displace some of your code
-				if client and client:supports_method('textDocument/inlayHint', event.buf) then
-					map('<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, 'Toggle Inlay [H]ints')
+				if client and client:supports_method("textDocument/inlayHint", event.buf) then
+					map("<leader>th", function()
+						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+					end, "Toggle Inlay [H]ints")
 				end
 			end,
 		})
 
-		-- Go fmt on save (uses modern vim.lsp.get_clients())
-		vim.api.nvim_create_autocmd('BufWritePre', {
-			pattern = '*.go',
-			callback = function()
-				local ok, err = pcall(function()
-					-- Prefer LSP formatting if gopls is available and supports formatting
-					local clients = vim.lsp.get_clients({ bufnr = 0 })
-					local has_gopls = false
-
-					for _, client in ipairs(clients) do
-						if client.name == 'gopls' and client.supports_method('textDocument/formatting') then
-							has_gopls = true
-							vim.lsp.buf.format({ timeout_ms = 2000 })
-							break
-						end
-					end
-
-					-- Fallback to external gofmt if no gopls formatting available
-					if not has_gopls then
-						vim.cmd('silent! %!gofmt')
-					end
-				end)
-
-				if not ok then
-					vim.notify('Go formatting failed: ' .. tostring(err), vim.log.levels.WARN)
-				end
-			end,
-		})
 		-- Enable the following language servers
 		---@type table<string, vim.lsp.Config>
 		local servers = {
@@ -117,21 +93,26 @@ return {
 				on_init = function(client)
 					if client.workspace_folders then
 						local path = client.workspace_folders[1].name
-						if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
+						if
+							path ~= vim.fn.stdpath("config")
+							and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+						then
+							return
+						end
 					end
 
-					client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
 						runtime = {
-							version = 'LuaJIT',
-							path = { 'lua/?.lua', 'lua/?/init.lua' },
+							version = "LuaJIT",
+							path = { "lua/?.lua", "lua/?/init.lua" },
 						},
 						workspace = {
 							checkThirdParty = false,
 							-- NOTE: this is a lot slower and will cause issues when working on your own configuration.
 							--  See https://github.com/neovim/nvim-lspconfig/issues/3189
-							library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
-								'${3rd}/luv/library',
-								'${3rd}/busted/library',
+							library = vim.tbl_extend("force", vim.api.nvim_get_runtime_file("", true), {
+								"${3rd}/luv/library",
+								"${3rd}/busted/library",
 							}),
 						},
 					})
@@ -140,7 +121,7 @@ return {
 					Lua = {},
 				},
 			},
-		}-- Ensure the servers and tools above are installed
+		} -- Ensure the servers and tools above are installed
 		--
 		-- You can press `g?` for help in this menu.
 		local ensure_installed = vim.tbl_keys(servers or {})
@@ -148,7 +129,7 @@ return {
 			-- You can add other tools here that you want Mason to install
 		})
 
-		require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 		for name, server in pairs(servers) do
 			vim.lsp.config(name, server)
@@ -156,4 +137,3 @@ return {
 		end
 	end,
 }
-
