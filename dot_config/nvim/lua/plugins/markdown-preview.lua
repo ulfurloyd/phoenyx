0b7c5ad91882702c39d34f9c51ca4067892981a1
ulfurloyd <eklavyasood@gmail.com>
2026-06-04 14:20:31 +0530

    nvim: keybindings

--- dot_config/nvim/lua/plugins/markdown-preview.lua
+++ dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -1,9 +1,10 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = { "markdown" },
-}
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	end,
+	ft = { "markdown" },
+},
+	vim.keymap.set("n", "<leader>tm", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle [m]arkdown Preview" })
--- dot_config/nvim/lua/plugins/vim-floaterm.lua
+++ dot_config/nvim/lua/plugins/vim-floaterm.lua
@@ -1,6 +1,5 @@
 return {
-	'voldikss/vim-floaterm',
+	"voldikss/vim-floaterm",
 },
-
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = "Exit terminal mode" }),
-vim.keymap.set('n', '<leader>tt', ':FloatermToggle<CR>', { desc = "Toggle floating [t]erminal" })
+	vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }),
+	vim.keymap.set("n", "<leader>tt", ":FloatermToggle<CR>", { desc = "Toggle floating [t]erminal" })
